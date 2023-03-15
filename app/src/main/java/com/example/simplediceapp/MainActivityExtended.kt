189fbe24eccc16a48ba9bed3c44d347ec722db4f
Pszczole <cezary932@gmail.com>
2023-03-15 14:40:55 +0100

    Simple android app that is created with AndroidStudio and uses kotlin.
    User is clicking a button and the images and the numbers of dices are shown, also color of text is changing.

---
+++ app/src/main/java/com/example/simplediceapp/MainActivityExtended.kt
@@ -0,0 +1,11 @@
+package com.example.simplediceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityExtended : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_extended)
+    }
+}
