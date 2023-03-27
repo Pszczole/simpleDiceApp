6b3f4e3cea1a39cfdae847f00bc9cad25f3a1fcc
Pszczole <cezary932@gmail.com>
2023-03-27 13:12:58 +0200

    Fourth commit: Updated MainActivityExtended.kt and SettingsActivity.kt

--- app/src/main/java/com/example/simplediceapp/MainActivityExtended.kt
+++ app/src/main/java/com/example/simplediceapp/MainActivityExtended.kt
@@ -1,12 +1,16 @@
 package com.example.simplediceapp
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.simplediceapp.databinding.ActivityMainExtendedBinding
 import com.google.android.material.snackbar.Snackbar
@@ -14,6 +18,18 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivityExtended : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainExtendedBinding
+    private var numDice: Int = 2 // number of dices that we use in game
+    private var isHoldEnabled: Boolean = true //flag: can user hold dice?
+    //Array of dice images ids for easy manipulation via loops
+    private val diceImgIdsArray = arrayOf(R.id.dice1ImgExtended,R.id.dice2ImgExtended,
+    R.id.dice3ImgExtended,R.id.dice4ImgExtended,R.id.dice5ImgExtended)
+    //Array to check when the dice is on Hold or Off
+    private val diceStateArray = arrayOf(false,false,false,false,false)
+    //Array of the values rolled with the dices
+    private val diceValuesArray = arrayOf(1,1,1,1,1)
+    private var currentPlayer = 0 // Current player id (Two players only)
+    private val playerScores = arrayOf(0,0) // Scores of the players
+    private var rollCount = 0 // variable that holds the number of rolls for each player
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i(localClassName,"onCreate")
@@ -21,11 +37,38 @@ class MainActivityExtended : AppCompatActivity() {
         //With view binding there we don't need to call findViewById
         binding = ActivityMainExtendedBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        applySettings()
+        //Set the click listener of the roll button
+        binding.rollButtonExtended.setOnClickListener{
+            //Check the state of the button by analysing its text
+            if(binding.rollButtonExtended.text != getString(R.string.button_enabled)){
+                //The text is not equal to Roll
+
+                binding.rollButtonExtended.text = getString(R.string.button_enabled)
+                if(currentPlayer == 1 && rollCount == 0){
+                    //if this is the start of the second player. reset turn
+                    resetTurn()
+                }
+            }
+
+        }
 
     }
 
-    private var numDice: Int = 2 // number of dices that we use in game
-    private var isHoldEnabled: Boolean = true //flag: can user hold dice?
+    //Function that converts our Int value to Image Resource
+    fun resolveDrawable(value: Int): Int{
+        return when (value){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_1
+        }
+    }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         //create a menu with menuInflater and R.menu.menu resource
@@ -54,6 +97,81 @@ class MainActivityExtended : AppCompatActivity() {
 
         //Start the SettingsActivity with the launchSettingsActivity variable
         launchSettingActivity.launch(intent)
+    }
+
+    private fun changeDiceTInt(img: ImageView, highlight:Boolean){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            //Change the tiny of the img. The getColor method is available for API >= M(API 23)
+            img.imageTintList =
+                ColorStateList.valueOf(getColor(if (highlight) R.color.myYellow
+                else R.color.white))
+        }else{
+            //For older API use getColor from resource class
+            @Suppress("DEPRECATION")// Annotation to disable the deprecation warning
+
+            img.imageTintList =
+                ColorStateList.valueOf(resources.getColor(if (highlight) R.color.myYellow
+                else R.color.white))
+        }
+    }
+    private fun resetTurn(){
+        // prepare game for the next turn
+        for(num in 1 .. 4){
+            diceValuesArray[num] = 1 // Reset the values of displayed by each dice
+            diceStateArray[num] = false // Reset the "hold state of each dice"
+            findViewById<ImageView>(diceImgIdsArray[num]).let {
+                changeDiceTInt(it, false) // reset the tint of each dice img view
+                it.setImageResource(resolveDrawable(1))
+            }
+        }
+
+        //Change the game label to show that user has changed
+        binding.playerLabel.apply {
+            text = getString(R.string.player_id, currentPlayer)
+        }
+    }
+
+
+    private fun resetGame(){
+        //Reset variables of the game nad Initialize labels and buttons
+        currentPlayer = 0
+        playerScores[0] = 0
+        playerScores[1] = 0
+        rollCount = 0
+        binding.rollResultTextExtended.text = getString(R.string.click_start)
+        binding.playerLabel.visibility = View.INVISIBLE
+        binding.rollButtonExtended.text = getString(R.string.button_initial)
+        resetTurn()
+    }
+    private fun applySettings(){
+        //Each time new settings are applied the game resets
+        binding.rollButtonExtended.isEnabled = true // enable button so the user can click it again
+        resetGame()
+
+        val diceToHideBegin = numDice + 1
+        //According to the numDice setting -> hide the remaining dice
+        for(num in 1 .. 5){
+            //Change the visibility of dices -> the view is found by its id
+            if(num in diceToHideBegin .. 5){
+                findViewById<ImageView>(diceImgIdsArray[num-1]).apply {
+                    //Making the visibility to GONE make it disappear and
+                    //not takee space in the layout
+                    visibility = View.GONE
+                    //Just in case disable any clicking on the image by disabling
+                    //clickable and focusable attribute
+                    isClickable = false
+                    isFocusable = false
+                }
+            }else{
+                findViewById<ImageView>(diceImgIdsArray[num - 1]).apply {
+                    //Make the image visible
+                    visibility = View.VISIBLE
+                    //make the image clickable
+                    isClickable = isHoldEnabled
+                    isFocusable = isHoldEnabled
+                }
+            }
+        }
     }
 
     //Register a callback for a SettingsActivity result.
--- app/src/main/java/com/example/simplediceapp/SettingsActivity.kt
+++ app/src/main/java/com/example/simplediceapp/SettingsActivity.kt
@@ -1,11 +1,52 @@
 package com.example.simplediceapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.example.simplediceapp.databinding.ActivityMainExtendedBinding
+import com.example.simplediceapp.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        //Initialize binding
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //Retrive the current settings of the game that were passed with
+        //Intent to this Activity
+        val numDice = intent.getIntExtra(getString(R.string.num_dice_key),2)
+        val isHoldEnabled = intent.getBooleanExtra(getString(R.string.hold_enable_key),true)
+        //Apply the current isHoldEnabled setting
+        binding.enableHoldSwitch.isChecked = isHoldEnabled
+        //Select the current num dices value form the spinner.
+        //We assume there are 5 dices max, and 2 min
+        if(numDice in 2 .. 5){
+            binding.numDiceSpinner.setSelection(numDice - 2)
+        }
+        //Set the click listener for confirm button
+        binding.confirmButton.setOnClickListener{
+            //get current selection from the spinner and convert it to integer number
+            //that will indicate the number of dices
+            val spinnerSelection = binding.numDiceSpinner.selectedItem.toString().toInt()
+            // get the current state of the hold enable switch
+            val holdEnable = binding.enableHoldSwitch.isChecked
+            //Create an empty intent that will be used to pass data to the calling Activity
+            val result = Intent().apply {
+                //Add the settings to the result intent
+                putExtra(getString(R.string.num_dice_key), spinnerSelection)
+                putExtra(getString(R.string.hold_enable_key), holdEnable)
+            }
+
+            //Set the resultCode to RESULT_OK and attach the data by the result Intent
+            setResult(RESULT_OK, result)
+            //Close this activity and go back to the previous one
+            finish()
+        }
+
+
+
     }
 }
